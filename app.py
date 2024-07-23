bc07ef6d6fb06b9dec9f80b67041eef9a9c3839b
liuivan615 <160048404+liuivan615@users.noreply.github.com>
2024-07-23 16:46:34 +0800

    app.py

--- app.py
+++ app.py
@@ -1,59 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/services')
-def services():
-    return render_template('services.html')
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    if request.method == 'POST':
-        # Handle form submission
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        # Do something with the form data (e.g., save to database, send email)
-        return redirect(url_for('thank_you'))
-    return render_template('contact.html')
-
 @app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
+def upload():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'essay' not in request.files:
-            return redirect(request.url)
-        file = request.files['essay']
-        # if user does not select file, browser also
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+        title = request.form['title']
+        file = request.files['file']
+        if file and title:
+            filename = title + "_" + file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file', filename=filename))
+            return redirect(url_for('index'))
     return render_template('upload.html')
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
